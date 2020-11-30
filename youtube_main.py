379a85f5d1a5cf06d70fee3a8de80d690f1a779f
yobleck <sennewell@gmail.com>
2020-11-30 09:40:47 -0500

    made file paths more robust
    now works no matter where the script is called from

--- youtube_main.py
+++ youtube_main.py
@@ -3,12 +3,17 @@ import time, os, sys;
 import curses, locale; 
 import yt_api_init, json, subprocess;
 
+
+cwd = sys.path[0] + "/";
 #logging tool
-#f = open("./log.txt","w"); f.write(str(test)); f.close();
+f = open(cwd + "log.txt","w"); f.write(str(sys.path)); f.close();
 
 def loading_scr(scr, mes):
     scr.addstr(0,0,mes);
     scr.refresh();
+
+#TODO: get rid of refresh loop and jsut add them on user inputs so it stop spamming refresh and allows for highlighting text
+#TODO: add colors
 
 def main(main_scr):
     ###initialization of curses
@@ -20,16 +25,17 @@ def main(main_scr):
     main_scr.keypad(True);
     
     
+    
     ###api initialization###
     loading_scr(main_scr,"Loading API's...");
     try:
         yt_api = yt_api_init.Init();
     except:
-        main_scr.addstr(0,0,"Something went wrong. Attempting clean exit...");
-        main_scr.refresh();
+        loading_scr(main_scr, "Something went wrong. Attempting clean exit...");
         time.sleep(1); #TODO: change to larger value when done
         curses.endwin();
-        return "youtube api error";
+        return "youtube api error: unable to load api";
+    
     
     
     ###api information download###
@@ -39,7 +45,7 @@ def main(main_scr):
     loading_scr(main_scr,"Loading YouTube Subscription Info...");
     #save subscriptions json to file if no file is found
     yt_subs = [];
-    if(len(os.listdir("./json/yt_subs/")) == 0):
+    if(len(os.listdir(cwd + "json/yt_subs/")) == 0):
         try:
             num_subs = yt_api.subscriptions().list(mine=True, part="contentDetails", maxResults=1).execute()["pageInfo"]["totalResults"];
             yt_sub_pages = [];#sub info first page
@@ -48,13 +54,12 @@ def main(main_scr):
                 yt_sub_pages.append( yt_api.subscriptions().list(mine=True, part="snippet", order="alphabetical", #sub info more pages
                                                             maxResults=50, pageToken=yt_sub_pages[i]["nextPageToken"]).execute() );
         except:
-            main_scr.addstr(0,0,"Something went wrong. Attempting clean exit...");
-            main_scr.refresh();
+            loading_scr(main_scr, "Something went wrong. Attempting clean exit...");
             time.sleep(1); #TODO: change to larger value when done
             curses.endwin();
-            return "could not get subscription information";
+            return "youtube api error: could not get subscription information";
         
-        f = open("./json/yt_subs/subs","w");
+        f = open(cwd + "json/yt_subs/subs","w");
         for i in yt_sub_pages:
             for j in i["items"]:
                 yt_subs.append(j["snippet"]);
@@ -62,9 +67,10 @@ def main(main_scr):
                 f.write("\n");
         f.close();
     else: #open subscription cache file ane read subs
-        f = open("./json/yt_subs/subs","r");
+        f = open(cwd + "json/yt_subs/subs","r");
         yt_subs = [json.loads(x) for x in f.readlines()];
     num_subs = len(yt_subs);
+    
     
     
     ###misc initialization###
@@ -74,17 +80,19 @@ def main(main_scr):
     ratings = {"108":"like", "100":"dislike", "110":"none"}; # maps user input to video rating action
     
     
+    
     ###initialization of screens
     home_scr = curses.newwin(term_h,term_w,0,0);
     yt_scr = curses.newwin(term_h,term_w,0,0);
-    sub_pad = curses.newpad(num_subs+1,term_w);
+    sub_pad = curses.newpad(num_subs+2,term_w);
     chnl_pad = curses.newpad(num_vids,term_w);
-    vid_scr = curses.newwin(term_h-1,term_w,1,0);
+    vid_scr = curses.newwin(term_h,term_w,0,0);
     
     #format: if len = 1 then [win]. if len > 1 then [pad,start_y, start_x, max_length, select_pos, list_var]
     fake_panel = [[home_scr], [yt_scr], [sub_pad,0,0,num_subs,0,yt_subs], [chnl_pad,0,0,num_vids,0,chnl_uplds], [vid_scr]];
     is_visible = [True]+[False]*4;
     in_focus = fake_panel[0];
+    
     
     
     ###screen set up###
@@ -107,7 +115,7 @@ def main(main_scr):
     yt_scr.addnstr(3,0, "Channel Birthday: " + my_channel_info["items"][0]["snippet"]["publishedAt"], term_w);
     
     #scrollable and clickable list of user subscriptions
-    sub_pad.addstr(0,0, "My YT Subscriptions", curses.A_ITALIC); #TODO: add notification of new videos on this page?
+    sub_pad.addstr(0,0, "My YT Subscriptions", curses.A_ITALIC | curses.A_UNDERLINE | curses.A_BOLD); #TODO: add notification of new videos on this page?
     for num, sub in enumerate(yt_subs):
         sub_pad.addnstr(num+1,0, sub["title"], term_w);
     
@@ -129,10 +137,8 @@ def main(main_scr):
             is_visible[0] = False; is_visible[1] = False; is_visible[3] = False; is_visible[4] = False;
             #subs page can be accessed from anywhere. hence hiding all other pages
         
-        #if(usr_input == 118 and is_visible[1] and is_visible[3]): #v
-            #is_visible[4] = True; in_focus = fake_panel[4];
         
-        
+        ###arrow key nav###
         if(usr_input == 258 and len(in_focus) > 1 and in_focus[4] < in_focus[3]-1): #scroll pad down with over scroll prevention
             in_focus[0].addnstr(in_focus[4]+1,0, in_focus[5][in_focus[4]]["title"], term_w);
             in_focus[4] += 1;
@@ -148,16 +154,24 @@ def main(main_scr):
                 in_focus[1] -= 1;
         
         
+        ###Enter###
         if(usr_input == 10):
             if(in_focus == fake_panel[2]): #selecting channel to display uploads
                 #get videos from yt api
-                channel = yt_api.channels().list(id=yt_subs[in_focus[4]]["resourceId"]["channelId"], part="contentDetails").execute();
-                json_uplds = yt_api.playlistItems().list(playlistId=channel["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"], part="snippet",maxResults=50).execute();
+                try:
+                    channel = yt_api.channels().list(id=yt_subs[in_focus[4]]["resourceId"]["channelId"], part="contentDetails").execute();
+                    json_uplds = yt_api.playlistItems().list(playlistId=channel["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"], part="snippet",maxResults=50).execute();
+                except:
+                    fake_panel[3][0].erase(); fake_panel[3][0].refresh(0,0,0,0,term_h-1,term_w);
+                    loading_scr(main_scr, "Something went wrong. Attempting clean exit...");
+                    time.sleep(1);
+                    curses.endwin();
+                    return "youtube api error: unable to get channel uploads";
                 
                 #prepare channel screen for drawing videos by resetting variables
                 fake_panel[3][0].erase();
                 fake_panel[3][1], fake_panel[3][2], fake_panel[3][4] = [0]*3;
-                fake_panel[3][0].addnstr(0,0, "Channel: " + yt_subs[in_focus[4]]["title"], term_w, curses.A_ITALIC);
+                fake_panel[3][0].addnstr(0,0, "Channel: " + yt_subs[in_focus[4]]["title"], term_w, curses.A_ITALIC | curses.A_UNDERLINE | curses.A_BOLD);
                 fake_panel[3][5] = [];
                 
                 #create list of videos
@@ -172,23 +186,31 @@ def main(main_scr):
             
             elif(in_focus == fake_panel[3]):
                 fake_panel[4][0].erase();
-                fake_panel[4][0].addnstr(0,0,"Video: " + in_focus[5][in_focus[4]]["title"], term_w);
-                fake_panel[4][0].addnstr(1,0,"Desc: " + in_focus[5][in_focus[4]]["description"].strip("\n"), term_w);
-                fake_panel[4][0].addnstr(2,0,"Thumbnail: " + in_focus[5][in_focus[4]]["thumbnails"]["maxres"]["url"], term_w);
+                fake_panel[4][0].addnstr(0,0,"Video: " + in_focus[5][in_focus[4]]["title"], term_w, curses.A_ITALIC | curses.A_UNDERLINE | curses.A_BOLD);
+                try:
+                    fake_panel[4][0].addnstr(2,0,"Thumbnail: " + in_focus[5][in_focus[4]]["thumbnails"]["maxres"]["url"], term_w);
+                except:
+                    fake_panel[4][0].addnstr(2,0,"Thumbnail: " + in_focus[5][in_focus[4]]["thumbnails"]["default"]["url"], term_w);
+                
                 #draw video url
                 fake_panel[4][0].addnstr(3,0,"url: https://www.youtube.com/watch?v=" + in_focus[5][in_focus[4]]["resourceId"]["videoId"], term_w);
+                fake_panel[4][0].addnstr(5,0,"Desc: " + in_focus[5][in_focus[4]]["description"].replace("\n"," "), term_w*(term_h-8));
+                
                 #draw like/dislike
                 fake_panel[4][0].addnstr(fake_panel[4][0].getmaxyx()[0]-1,0, "Like/Dislike status: " + yt_api.videos().getRating(id=in_focus[5][in_focus[4]]["resourceId"]["videoId"]).execute()["items"][0]["rating"], term_w);
                 is_visible[4] = True; in_focus = fake_panel[4]; is_visible[3] = False;
+        
         
         
         if(usr_input == 32 and in_focus == fake_panel[4]): #space bar to play video
             subprocess.run([ "mpv","https://www.youtube.com/watch?v="+fake_panel[3][5][fake_panel[3][4]]["resourceId"]["videoId"] ], stdout=subprocess.DEVNULL);
         
         
+        
         if(usr_input in [108,100,110]): #like/dislike/none with l,d,n
             yt_api.videos().rate(id=fake_panel[3][5][fake_panel[3][4]]["resourceId"]["videoId"], rating=ratings[str(usr_input)]).execute();
             fake_panel[4][0].addnstr(fake_panel[4][0].getmaxyx()[0]-1,0,"Like/Dislike status: " + ratings[str(usr_input)] + "   ", term_w);
+        
         
         
         if(usr_input == 127): #backspace to go back a page
@@ -200,6 +222,7 @@ def main(main_scr):
                 is_visible[0] = True; in_focus = fake_panel[0]; is_visible[1] = False; is_visible[2] = False;
         
         
+        
         #redraw loop checks what is visible and draws in the correct order. slow but nothing else tried worked.
         for x in range(5):
             if(fake_panel[x] == in_focus):#if(is_visible[x]):
@@ -207,12 +230,12 @@ def main(main_scr):
                 try:
                     fake_panel[x][0].refresh(); #windows
                 except curses.error:
-                    fake_panel[x][0].refresh(fake_panel[x][1],fake_panel[x][2], 1,0, term_h-1,term_w); #pads
+                    fake_panel[x][0].refresh(fake_panel[x][1],fake_panel[x][2], 0,0, term_h-1,term_w); #pads
         
         time.sleep(0.01);
     #end while loop
     
-    return "clean exit, no error";
+    return "clean exit, no errors";
 #end main
 
 ###actual running of code
--- yt_api_init.py
+++ yt_api_init.py
@@ -1,5 +1,5 @@
 #initiliaze youtube api
-import os;
+import os, sys;
 
 import google_auth_oauthlib.flow;
 import googleapiclient.discovery;
@@ -9,15 +9,16 @@ import google.auth.transport.requests;
 
 
 def Init():
+    cwd = sys.path[0] + "/";
     scopes = ["https://www.googleapis.com/auth/youtube"]; #change youtube.readonly to just youtube
-    credentials_file = "/home/yobleck/yt-py-ncurses/json/credentials.json"; #make this come from config file? also encrypt it?
+    credentials_file = cwd + "json/credentials.json"; #make this come from config file? also encrypt it?
     
     # Disable OAuthlib's HTTPS verification when running locally.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"; # *DO NOT* leave this option enabled in production.
 
     api_service_name = "youtube";
     api_version = "v3";
-    client_secrets_file = "/home/yobleck/yt-py-ncurses/json/client_secret_915368223513-6bt98h496rv56tj4r2uq57te33i7he3p.apps.googleusercontent.com.json";
+    client_secrets_file = cwd + "json/client_secret_915368223513-6bt98h496rv56tj4r2uq57te33i7he3p.apps.googleusercontent.com.json";
 
     # Get client key and secrets and create an API client
     #flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -29,7 +30,7 @@ def Init():
         credentials = flow.run_local_server(); #credentials = flow.run_console();
         cred_json = credentials.to_json();
         print("cred_json:\n", cred_json);
-        f = open("./json/credentials.json","w");
+        f = open(cwd + "json/credentials.json","w");
         f.write(cred_json);
         f.close();
     
@@ -40,7 +41,7 @@ def Init():
         credentials.refresh(google.auth.transport.requests.Request()); # does none need to be there?
         cred_json = credentials.to_json();
         #print("cred_json:\n", cred_json);
-        f = open("./json/credentials.json","w");
+        f = open(cwd + "json/credentials.json","w");
         f.write(cred_json);
         f.close();
     
